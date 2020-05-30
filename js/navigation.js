54ebd38d216360c55c67b3f1cd853054d9ac73ac
tatjama <tanja120a@gmail.com>
2020-05-30 20:14:03 +0200

    responsive navigation styling

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@ function openMenu(){
     document.querySelector('.nav-hamburger').style.display = 'none';
     document.querySelector('.nav-close').style.display = 'initial';
     document.querySelector('.nav-menu').style.display = 'initial';
-    document.querySelector('.section-one-svg').style.visibility = 'hidden';
+   document.querySelector('.nav-bg').style.display = 'initial';
     document.querySelector('.nav-bg-menu').style.display = 'initial';      
 }
 function closeMenu(){
