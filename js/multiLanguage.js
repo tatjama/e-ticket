6ad8c99b1c2c9554f27626f23f9f37ffc78e8518
tatjama <tanja120a@gmail.com>
2020-06-24 07:57:33 +0200

    Translate landing page done

--- js/multiLanguage.js
+++ js/multiLanguage.js
@@ -35,6 +35,8 @@ var arrLang = {
         'termsContent' : 'Quick, easy and safe purchase of tickets for theater, ballet, opera or philharmonic concerts. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' ,
         'customerReturn': ' Ticket replacement ',
         'customerReturnContent' : 'We all sometimes find ourselves in an unplanned situation when our only option is to return the purchased ticket. These are the conditions for a refund... Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis',
+        'entry' : 'Entry',
+        'sale' : 'Sale',
         'show-more': 'show more...',
         'show-less' : '...show less',
         'signUpLegend': 'SIGN UP',
@@ -71,6 +73,8 @@ var arrLang = {
         'termsContent' : 'Brza, jednostavna i sigurna kupovina karata za pozorište, balet, operu ili koncerte filharmonije. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' ,
         'customerReturn' : ' Uslovi zamene karata',
         'customerReturnContent' : 'Svi se ponekad nađemo u neplaniranoj situaciji kada nam nam je jedina opcija vraćanje kupljene ulaznice. Ovo su uslovi za povrat novca... Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis',
+        'entry' : 'Ulaz',
+        'sale' : 'Prodaja',
         'show-more' : 'saznaj više...',
         'show-less' : '...saznaj manje',
         'signUpLegend': 'REGISTRACIJA KORISNIKA',
@@ -105,22 +109,31 @@ var arrLang = {
 var arrLangAt = {
     'en':{
         'name': 'Yor name...',
+        'error name' : 'Error. Type valid name...',
+        'error surname': 'Error. Type valid surname',
         'surname' : 'Your surname..',
+        'error email' : 'Error. Type valid E-mail',
         'password' : 'Password...',
+        'error pssword' : 'Error. Type valid password',
         'register' : 'Register',
         'clear' : 'Clear',
         'signIn' : 'Sign in',
         'type comment' : 'Type comment...',
+        'news' : 'News in your inbox...'
     },
     'sr' :{
         'name' : 'Unesite ime..',
+        'error name' : 'Pogrešan unos. Unesite validno ime',
+        'error surname' : 'Pogrešan unos. Unesite validno prezime',
         'surname' : 'Unesite prezime..',
-        'password' : 'Unesite ozinku...',
+        'error email' : 'Pogrešan unos. Unesite validan E-mail',
+        'password' : 'Unesite lozinku...',
+        'error password' : 'Pogrešan unos. Unesite validnu lozinku',
         'register' : 'Registracija',
         'clear' : 'Obriši',
         'signIn' : 'Uloguj',
         'type comment' : 'Napišite komentar...', 
-
+        'news' : "Novosti u Vašem sandučetu…"
     }
 };
 $(function(){
@@ -138,6 +151,10 @@ $(function(){
     // value attribute value - change
         $('.langVal').each(function(index, element){
             $(this).attr('value', arrLangAt[lang][$(this).attr('key')])
-        })
+        });
+    // value attribute title - change
+        $('.langTitle').each(function(index, element){
+            $(this).attr('title', arrLangAt[lang][$(this).attr('key')])
+        });
     })
 })
