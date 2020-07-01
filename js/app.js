4d1c24429f44daf276dd689161618d25ee4ee34d
tatjama <tanja120a@gmail.com>
2020-07-01 15:22:43 +0200

    inner html display on index.html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//getting tickets
 class Tickets{
     async getTickets(){
         try{
@@ -15,8 +16,45 @@ class Tickets{
     }
 }
 
+//display tickets
+class UI{
+
+    displayTickets(tickets){
+        console.log(tickets)
+        let result = '';
+        tickets.forEach((ticket)=>{
+            result += `
+            <div class="premiere">
+                  
+                   <img  alt="balet1" src="./images/webp/${ticket.image}">
+                  
+                  <div class="parallax- text"> 
+                    <a href="./view/eshop.html">
+                    <h4 >${ticket.title}</h4> </a>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tristique diam in tempor condimentum. Proin condimentum a purus non malesuada. Nulla et dignissim libero, a accumsan enim. Quisque at auctor dolor. Praesent vitae neque iaculis,
+                      molestie sapien quis, pretium est. Sed vehicula                      
+                    </p>
+                    <h5> <span class="lang" key="date"> DATE: </span> ${ticket.date}</h5>
+                      <h5><span class="lang" key = "time">TIME: </span> 20:00h</h5>
+                      <h5><span class="lang" key = 'scene'>SCENE: </span> ${ticket.scene}</h5>
+                    </div>
+                       
+                  </div> 
+            `;
+          // document.querySelector('#section_three_h1_result').innerHTML = result;
+
+        })
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded',()=>{
+    
     const tickets = new Tickets();
-    tickets.getTickets().then((data)=>console.log(data))
+    const ui = new UI();
+    //get tickets
+    tickets.getTickets().then((tickets)=>{
+        ui.displayTickets(tickets)
+    })
    // console.log(tickets.getTickets())
 })
