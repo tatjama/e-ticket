808808b4f924c991a1ca6cbc45f910b016903487
tatjama <tanja120a@gmail.com>
2020-07-22 14:56:55 +0200

    navigation.js added to index.js
    navigation.js added to index.ja, and removed from files

--- js/index.js
+++ js/index.js
@@ -6,22 +6,57 @@ function translateL(x){
  return currentlyLanguage
 }
 
+let hamburgerElement = document.querySelector('.nav-hamburger');
+let closeElement = document.querySelector('.nav-close');
+let navBgMenuElement = document.querySelector('.nav-bg-menu');
+let signInMobileElement = document.getElementById('signIn_mobile');
+let signUpMobileElement = document.getElementById('signUp_mobile');
+let signingMobileElement =  document.getElementById('signing_mobile');
+
+function openMenu(){    
+    hamburgerElement.style.display = 'none';
+    closeElement.style.visibility = 'show';
+    navBgMenuElement.style.display = 'initial';   
+   // document.getElementById("start_mobile").style.display = "none";
+    signingMobileElement.style.display= "inherit";
+    signInMobileElement.style.display = "block";
+    signUpMobileElement.style.display = "block";
+}
+function closeMenu(){
+    hamburgerElement.style.display = "initial";
+    closeElement.style.visibility = 'hide';
+    navBgMenuElement.style.display = 'none';
+    signInMobileElement.style.display = "none";
+    signUpMobileElement.style.display = "none";
+}
+
+function checkEMail(){       
+
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+   {    
+     document.getElementById('form-message').innerHTML= "ok";
+     return (true)    
+   }
+      document.getElementById('form-message').innerHTML = "Please insert a valid email";
+     return (false)  
+   }
+
 let pickUpSignInElement = document.getElementById("pickUp_signIn");
 let pickUpSignUpElement = document.getElementById("pickUp_signUp");
+
+
 //pop-up prozor logovanje
 function showLoginForm() {
   pickUpSignInElement.style.display = "block";
 }
-
-
 function hideLoginForm() {
   pickUpSignInElement.style.display = "none";
 }
+
 //pop-up prozor registracija
 function showSignUpForm() {
   pickUpSignUpElement.style.display = "block";
 }
-
 function hideSignUpForm() {
   pickUpSignUpElement.style.display = "none";
 }
