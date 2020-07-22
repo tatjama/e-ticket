e7676cec5e7b19153612e71c69813057f03176cb
tatjama <tanja120a@gmail.com>
2020-07-22 14:42:09 +0200

    Update navigation.js
    variable initialisation

--- js/navigation.js
+++ js/navigation.js
@@ -1,18 +1,25 @@
+let hamburgerElement = document.querySelector('.nav-hamburger');
+let closeElement = document.querySelector('.nav-close');
+let navBgMenuElement = document.querySelector('.nav-bg-menu');
+let signInMobileElement = document.getElementById('signIn_mobile');
+let signUpMobileElement = document.getElementById('signUp_mobile');
+let signingMobileElement =  document.getElementById('signing_mobile');
+
 function openMenu(){    
-    document.querySelector('.nav-hamburger').style.display = 'none';
-    document.querySelector('.nav-close').style.visibility = 'show';
-    document.querySelector('.nav-bg-menu').style.display = 'initial';   
-    document.getElementById("start_mobile").style.display = "none";
-    document.getElementById('signing_mobile').style.display = "inherit";
-    document.getElementById('signIn_mobile').style.display = "block";
-    document.getElementById('signUp_mobile').style.display = "block";
+    hamburgerElement.style.display = 'none';
+    closeElement.style.visibility = 'show';
+    navBgMenuElement.style.display = 'initial';   
+   // document.getElementById("start_mobile").style.display = "none";
+    signingMobileElement.style.display= "inherit";
+    signInMobileElement.style.display = "block";
+    signUpMobileElement.style.display = "block";
 }
 function closeMenu(){
-    document.querySelector('.nav-hamburger').style.display = "initial";
-    document.querySelector('.nav-close').style.visibility = 'hide';
-    document.querySelector('.nav-bg-menu').style.display = 'none';
-    document.getElementById('signIn_mobile').style.display = "none";
-    document.getElementById('signUp_mobile').style.display = "none";
+    hamburgerElement.style.display = "initial";
+    closeElement.style.visibility = 'hide';
+    navBgMenuElement.style.display = 'none';
+    signInMobileElement.style.display = "none";
+    signUpMobileElement.style.display = "none";
 }
 /*function currentSlide(n){         
     document.querySelector('.slider').style.left =185-n*375 +'px';          
